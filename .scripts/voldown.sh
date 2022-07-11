d2d137efa6fb9d42c7d195530024d2b42c55d6e9
Ryscith <rpschultz1123@gmail.com>
2022-07-10 23:23:01 -0400

    Added custom scripts

---
+++ .scripts/voldown.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+source ~/.dwm/update_bar.sh
+
+/usr/bin/pactl set-sink-volume @DEFAULT_SINK@ -5%
+
+mute="$(/usr/bin/pactl get-sink-mute @DEFAULT_SINK@ | awk '{print $2}')"
+vol=$(/usr/bin/pactl get-sink-volume @DEFAULT_SINK@ | awk '{print $5}' | sed 's/%//')
+bar=$(seq -s "─" $(($vol / 5)) |  sed 's/[0-9]//g')
+
+if [[ "$mute" == "no" ]]
+then
+    dunstify -i /usr/share/icons/Papirus-Dark/16x16/panel/volume-level-high-panel.svg -r 2593 -u normal "   $bar"
+else
+    dunstify -i /usr/share/icons/Papirus-Dark/16x16/panel/volume-level-muted-panel.svg -r 2593 -u normal "   $bar"
+fi
+
+update_bar
