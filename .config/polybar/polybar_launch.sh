65e70caf7f67bf95ff04a898033ccda564c9b147
Reilly Schultz <rpschultz1123@gmail.com>
2022-10-10 18:53:33 -0400

    Removed old configs I no longer needed

--- .config/polybar/polybar_launch.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-# Terminate any running instances
-killall -q polybar
-
-# Make sure Xresources contains wall colors before launching
-xrdb -merge ~/.cache/wal/colors.Xresources &&
-
-polybar --config=~/.config/polybar/rounded_gaps_bar main & disown
